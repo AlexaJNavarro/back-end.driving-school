f176dcbdf0f27f535abd54d91fbdc6279c821a97
AlexaJNavarro <ale2002.nn13@gmail.com>
2021-06-11 04:25:28 -0500

    update

--- src/controller/notes.controller.ts
+++ src/controller/notes.controller.ts
@@ -39,16 +39,47 @@ export class NotesController{
 
     public static async Create(req:Request, res:Response):Promise<Response>{
         const body:NotesInterface = req.body
-        if(body._id.length==24){
+        if(body.id_teacher.length == 24 && body.id_student.length==24){
+            if(typeof body.notes.simulacrum === 'number' && typeof body.notes.hours === 'number' && typeof body.notes.theoric_exam === 'number' && typeof body.notes.practice_exam === 'number'){
+                try {
+                    const notes = await NotesModel.Create(body)
+                    const response = new Answer('Message',"Se registro correctamente",false,notes)
+                    return res.status(201).json(response)
+                } catch (error) {
+                    const response = new Answer('Error',error,true,null)
+                    return res.status(500).json(response)
+                }
+            }else{
+                const response = new Answer('Error',"No es valido los datos ingresados de la colección Notas",true,null)
+                return res.status(404).json(response)
+            }
+
+        }else{
+            const response = new Answer('Error',"No es valido el id del profesor o del estudiante",true,null)
+            return res.status(404).json(response)
+        }
+
+    }
+    public static async Update(req:Request, res:Response):Promise<Response>{
+        const id = req.params.ID
+        const body:NotesInterface = req.body
+
+        if(id.length==24){
             if(body.id_teacher.length == 24 && body.id_student.length==24){
                 if(typeof body.notes.simulacrum === 'number' && typeof body.notes.hours === 'number' && typeof body.notes.theoric_exam === 'number' && typeof body.notes.practice_exam === 'number'){
                     try {
-                        const notes = await NotesModel.Create(body)
-                        const response = new Answer('Message',"Se registro correctamente",false,notes)
-                        return res.status(201).json(response)
+            
+                        const notes = await NotesModel.Update(id, body)
+                        if(notes != null){
+                            const response = new Answer('Message',"Se actualizo correctamente",false,notes)
+                            return res.status(200).json(response)
+                        }else{
+                            const response = new Answer('Error',"El id es incorrecto ",false,null)
+                            return res.status(404).json(response)
+                        }
                     } catch (error) {
                         const response = new Answer('Error',error,true,null)
-                        return res.status(404).json(response)
+                        return res.status(500).json(response)
                     }
                 }else{
                     const response = new Answer('Error',"No es valido los datos ingresados de la colección Notas",true,null)
@@ -64,58 +95,45 @@ export class NotesController{
             const response = new Answer('Error',"No es valido el id",true,null)
             return res.status(404).json(response)
         }
-    }
-    public static async Update(req:Request, res:Response):Promise<Response>{
-        // if(body._id.length==24){
-        //     if(body.id_teacher.length == 24 && body.id_student.length==24){
-        //         if(typeof body.notes.simulacrum === 'number' && typeof body.notes.hours === 'number' && typeof body.notes.theoric_exam === 'number' && typeof body.notes.practice_exam === 'number'){
-
-        //         }else{
-        //             const response = new Answer('Error',"No es valido los datos ingresados de la colección Notas",true,null)
-        //             return res.status(404).json(response)
-        //         }
-
-        //     }else{
-        //         const response = new Answer('Error',"No es valido el id del profesor o del estudiante",true,null)
-        //         return res.status(404).json(response)
-        //     }
-
-        // }else{
-        //     const response = new Answer('Error',"No es valido el id",true,null)
-        //     return res.status(404).json(response)
-        // }
-        try {
-            const id = req.params.ID
-            const body:NotesInterface = req.body
-            const notes = await NotesModel.Update(id, body)
-            const response = new Answer('Message',"Se actualizo correctamente",false,notes)
-            return res.status(200).json(response)
-        } catch (error) {
-            const response = new Answer('Error',error,true,null)
-            return res.status(404).json(response)
-        }
-
+        
     }
     public static async Delete(req:Request, res:Response):Promise<Response>{
-        try {
-            const id = req.params.ID
-            const notes = await NotesModel.Delete(id)
-            const response = new Answer("Message", "Se elimino el registro", false, notes)
-            return res.status(200).json(response)
-        } catch (error) {
-            const response = new Answer("Error", error, true, null)
-            return res.status(200).json(response)
+        const id = req.params.ID
+        if(id.length==24){
+            try {
+                const notes = await NotesModel.Delete(id)
+                if(notes.deletedCount != 0){
+                    const response = new Answer("Message", "Se elimino el registro", false, notes)
+                    return res.status(200).json(response)
+                }else{
+                    const response = new Answer('Error',"El id es incorrecto ",false,null)
+                    return res.status(404).json(response)
+                }
+            } catch (error) {
+                const response = new Answer("Error", error, true, null)
+                return res.status(200).json(response)
+            }
+        }else{
+            const response = new Answer('Error',"No es valido el id",true,null)
+            return res.status(404).json(response)
         }
     }
     public static async DeleteAll(req:Request, res:Response):Promise<Response>{
-        try {
-            const body:Array<object> = req.body
-            const notes = await NotesModel.DeleteAll(body)
-            const response = new Answer("Message", "Se elimino todos los registro", false, notes)
-            return res.status(200).json(response)
-        } catch (error) {
-            const response = new Answer("Error", error, true, null)
-            return res.status(200).json(response)
+        const body:Array<object> = req.body
+        if(body!=null){
+            try {
+                const notes = await NotesModel.DeleteAll(body)
+                const response = new Answer("Message", "Se elimino todos los registro", false, notes)
+                return res.status(200).json(response)
+            } catch (error) {
+                const response = new Answer("Error", error, true, null)
+                return res.status(500).json(response)
+            }
+
+        }else{
+            const response = new Answer("Error", "Los datos enviados, son nulos", true, null)
+            return res.status(404).json(response)
         }
+        
     }
 }
